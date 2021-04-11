8f9b27f9468f9fceada025bd4f5bcb6365a0e058
vaibhavarya1622 <vaibhavarya1622@gmail.com>
2021-04-11 12:58:50 +0530

    first update

---
+++ main.py
@@ -0,0 +1,47 @@
+from tkinter import *
+import train
+import face_recognition
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import itertools
+
+def open_train():
+    train.train()
+def open_detect():
+    face_recognition.recognition(inputform.get())
+
+
+
+wb=Workbook()
+wb=load_workbook('Attendance.xlsx')
+ws=wb.active
+
+col_a=ws['A']
+col_b=ws['B']
+
+def get_name():
+    lista=''
+    for (cell,cell2) in zip(col_a,col_b):
+      lista=f'{lista +str(cell.value)+" "+str(cell2.value)}\n'
+    labeldisplay.config(text=lista)
+
+root=Tk()
+root.geometry("500x800")
+root.title('Attendance System')
+labela=Label(root,text="ATTENDANCE SYSTEM")
+labela.pack(pady="20")
+labelentry=Label(root,text="enter file path")
+inputform=Entry(root,text="enter data")
+inputform.pack(pady="20")
+
+buttontrain=Button(root,text="train model",command=open_train)
+buttontrain.pack(pady=20)
+buttondetect=Button(root,text="detect model",command=open_detect)
+buttondetect.pack(pady=20)
+
+buttondisplay=Button(root,text="display attendance",command=get_name)
+buttondisplay.pack(pady=20)
+labeldisplay=Label(root,text="")
+labeldisplay.pack(pady=20)
+
+root.mainloop()
